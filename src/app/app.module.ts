417dd23bf69ecb9dc4e15f98e41c554839a884b6
wizwoz <wht2@st-andrews.ac.uk>
2021-01-29 21:32:26 +0000

    back to sqaure one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { InlineSVGModule } from 'ng-inline-svg';
 
-
 import { AppComponent } from './app.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { WorkComponent } from './work/work.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExperienceComponent,
-    AboutMeComponent,
-    WorkComponent,
     HomeComponent
   ],
   imports: [
